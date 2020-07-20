a63fded8bfac8c93723f0ccfdad53e49fbc9d89e
Gklam <khalid.lamsadi@outlook.fr>
2020-07-20 01:24:16 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CubeMail/precache-manifest.414d75f183a9332e1d3fd0c764b86700.js"
+  "/CubeMail/precache-manifest.c8f627178853ee7b2c923833a7ca28a1.js"
 );
 
 self.addEventListener('message', (event) => {
